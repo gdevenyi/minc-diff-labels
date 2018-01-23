d4064acb9e6cb1a63f61c5549e37fd02178de865
Gabriel A. Devenyi <gdevenyi@gmail.com>
2018-01-23 11:24:53 -0500

    Streamline patch storage, fix cases where label is missing in patch or label

--- diff-labels.sh
+++ diff-labels.sh
@@ -9,17 +9,39 @@ old=$1
 new=$2
 output=$3
 
-labels=$(print_all_labels $old | cut -d " " -f 2)
-for label in ${labels}
+oldlabels=$(print_all_labels $old | cut -d " " -f 2)
+newlabels=$(print_all_labels $new | cut -d " " -f 2)
+combinedlabels=$(echo ${oldlabels} ${newlabels} | tr ' ' '\n' | sort | uniq)
+
+for label in ${oldlabels}
 do
     minclookup -discrete -lut_string "$label 1" $old ${tmpdir}/${label}_old.mnc
+done
+
+for label in ${newlabels}; do
     minclookup -discrete -lut_string "$label 1" $new ${tmpdir}/${label}_new.mnc
+done
+
+changedlabels=""
+
+for label in ${combinedlabels}; do
+    if [[ ! -e ${tmpdir}/${label}_old.mnc ]]; then
+        minccalc -expression 'A[0]?0:0' -byte -signed ${tmpdir}/${label}_new.mnc ${tmpdir}/${label}_old.mnc
+    elif [[ ! -e ${tmpdir}/${label}_new.mnc ]]; then
+        minccalc -expression 'A[0]?0:0' -byte -signed ${tmpdir}/${label}_old.mnc ${tmpdir}/${label}_new.mnc
+    fi
+            
     minccalc -labels -signed -byte -expression 'A[0] - A[1]' ${tmpdir}/${label}_new.mnc ${tmpdir}/${label}_old.mnc ${tmpdir}/${label}_diff.mnc
-    rm ${tmpdir}/${label}_old.mnc ${tmpdir}/${label}_new.mnc
+    if [[ ! (( $(mincstats -quiet -sum2 ${tmpdir}/${label}_diff.mnc) >  0)) ]]; then
+        rm ${tmpdir}/${label}_diff.mnc
+    else
+        changedlabels+="${label} "
+    fi
 done
 
 
-mincconcat -valid_range "-1 1" -signed -byte -concat_dimension time -coordlist "$(echo ${labels} | tr ' ' '\n' | paste -s -d",")" $(for label in $labels; do echo -n "${tmpdir}/${label}_diff.mnc "; done ) ${output}
+
+mincconcat -valid_range -1 1 -signed -byte -concat_dimension time -coordlist "$(echo ${changedlabels} | tr ' ' '\n' | paste -s -d",")" $(for label in ${changedlabels}; do echo -n "${tmpdir}/${label}_diff.mnc "; done ) ${output}
 
 
 rm -rf ${tmpdir}
--- patch-labels.sh
+++ patch-labels.sh
@@ -9,26 +9,35 @@ old=$1
 patch=$2
 output=$3
 
-labels=$(print_all_labels $old | cut -d " " -f 2)
-patchlabels=$(mincinfo -varvalues time $patch)
-combinedlabels=$(echo ${labels} ${patchlabels} | tr ' ' '\n' | sort | uniq)
+oldlabels=$(print_all_labels ${old} | cut -d " " -f 2)
+patchlabels=$(mincinfo -varvalues time ${patch})
+combinedlabels=$(echo ${oldlabels} ${patchlabels} | tr ' ' '\n' | sort | uniq)
 
-for label in ${labels}; do
+#Split up original label file
+for label in ${oldlabels}; do
     minclookup -discrete -lut_string "$label 1" ${old} ${tmpdir}/${label}_old.mnc
 done
 
-i=1
-for patchlabel in ${patchlabels}; do
-    mincreshape -valid_range -1 1 ${patch} ${tmpdir}/${patchlabel}_patch.mnc -dimrange time=${i} -signed -byte
+#Split up patch set
+i=0
+for label in ${patchlabels}; do
+    mincreshape -valid_range -1 1 ${patch} ${tmpdir}/${label}_patch.mnc -dimrange time=${i} -signed -byte
     #Generate an empty file if not present in existing label set so we can patch it
-    if [[ ! -e ${tmpdir}/${patchlabel}_old.mnc ]]; then
-    minccalc -expression 'A[0]?0:0' -byte -signed ${tmpdir}/${patchlabel}_patch.mnc ${tmpdir}/${patchlabel}_old.mnc
-    fi
-    ((i++))
+    ((i++)) || true
 done
 
+#Apply patch to labels, in the case of missing label or patch, skip patching
 for label in ${combinedlabels}; do
-    minccalc -labels -unsigned -byte -expression "(A[0]+A[1])*${label}" ${tmpdir}/${label}_old.mnc ${tmpdir}/${label}_patch.mnc ${tmpdir}/${label}_new.mnc
+    if [[ ! -e ${tmpdir}/${label}_old.mnc ]]; then
+        if [[ $(mincstats -min -quiet ${tmpdir}/${label}_patch.mnc) == "-1" ]]; then
+            echo "Warning, patch ${label} deletes voxels but no original label exists! Ignoring negative patches!"
+        fi
+        minccalc -labels -unsigned -byte -expression "A[0]>0?(A[0]*${label}):0" ${tmpdir}/${label}_patch.mnc ${tmpdir}/${label}_new.mnc
+    elif [[ ! -e ${tmpdir}/${label}_patch.mnc ]]; then
+        minccalc -labels -unsigned -byte -expression "A[0]*${label}" ${tmpdir}/${label}_old.mnc ${tmpdir}/${label}_new.mnc
+    else
+        minccalc -labels -unsigned -byte -expression "(A[0]+A[1])*${label}" ${tmpdir}/${label}_old.mnc ${tmpdir}/${label}_patch.mnc ${tmpdir}/${label}_new.mnc
+    fi
 done
 
 mincmath -labels -unsigned -byte -add ${tmpdir}/*_new.mnc ${output}
